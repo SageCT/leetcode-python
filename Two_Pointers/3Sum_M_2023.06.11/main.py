5cebd39c44fce3c364f3004fbbac337bf2cbdcf5
SageCT <egas6554@gmail.com>
2023-06-12 18:43:46 -0500

    Explanation for 3Sum added

--- Two_Pointers/3Sum_M_2023.06.11/main.py
+++ Two_Pointers/3Sum_M_2023.06.11/main.py
@@ -1,5 +1,4 @@
 from typing import List
-import numpy as np
 
 
 # * Remember that this is a two pointer problem,
@@ -9,21 +8,34 @@ class Solution:
         nums.sort()
         ans: List[List[int]] = []
 
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
+        for i in range(len(nums) - 2):
+            # ? To skip over duplicates, because we sorted the list
+            # ? we can check if the current number is the same as the previous number
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                three_sum = a + nums[l] + nums[r]
-                if three_sum > 0:
-                    r -= 1
-                elif three_sum < 0:
-                    l += 1
-                else:
-                    ans.append([a, nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
 
-        # ans = np.unique(ans).tolist()
+            # ? We set the left pointer to be one ahead of the current number
+            # ? and the right pointer to be at the end of the list
+            left = i + 1
+            right = len(nums) - 1
+
+            # ? We iterate until the left and right pointers meet
+            # ? If left is greater than right, then we have checked all possible combos
+            # ? For this iteration of i
+            while left < right:
+                three_sum = nums[i] + nums[left] + nums[right]
+                if three_sum < 0:
+                    left += 1
+                elif three_sum > 0:
+                    right -= 1
+                else:
+                    # ? If the sum is 0, then we add the numbers to the ans list and move the pointers
+                    # ? We also check if the next number is the same as the current number
+                    ans.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+                    while nums[right] == nums[right + 1] and left < right:
+                        right -= 1
         return ans
