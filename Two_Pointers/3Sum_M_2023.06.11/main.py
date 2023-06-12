543451e5756aa516250cd761f111d76e6fb7e0ff
SageCT <egas6554@gmail.com>
2023-06-12 13:53:19 -0500

    3Sum solved, need to understand solution

--- Two_Pointers/3Sum_M_2023.06.11/main.py
+++ Two_Pointers/3Sum_M_2023.06.11/main.py
@@ -6,16 +6,24 @@ import numpy as np
 # * chances are you start at opposite ends of the list and iterate towards each other
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         ans: List[List[int]] = []
 
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums) // 2):
-                for k in range(j + 1, len(nums)):
-                    if (
-                        nums[i] + nums[j] + nums[k] == 0
-                        and sorted([nums[i], nums[j], nums[k]]) not in ans
-                    ):
-                        ans.append(sorted([nums[i], nums[j], nums[k]]))
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                three_sum = a + nums[l] + nums[r]
+                if three_sum > 0:
+                    r -= 1
+                elif three_sum < 0:
+                    l += 1
+                else:
+                    ans.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
 
         # ans = np.unique(ans).tolist()
         return ans
