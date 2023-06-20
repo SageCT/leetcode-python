530b065c1144ec7c9ce0e8f0f5b7b3a8920668f5
SageCT <egas6554@gmail.com>
2023-06-19 21:08:59 -0500

    Added Reverse Linked List

--- Linked_List/ReverseLinkedList_E_2023.06.13/main.py
+++ Linked_List/ReverseLinkedList_E_2023.06.13/main.py
@@ -9,6 +9,14 @@ class ListNode:
 
 
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        while head.next is not None:
-            head = head.next
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        return prev
