3cfae4f2758f7f49f24595b4f3a58ac9d5ea15d8
SageCT <egas6554@gmail.com>
2023-07-05 01:02:22 -0500

    Updated problem153

--- Binary_Search/main.py
+++ Binary_Search/main.py
@@ -1,6 +1,21 @@
 from typing import List
 
+# class Solution:
+#     def findMin(self, nums: List[int]) -> int:
+#         return min(nums)
+
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        return min(nums)
+        left, right = 0, len(nums) - 1
+        min_num = nums[0]
+
+        while left < right:
+            mid = (left + right) // 2
+            min_num = min(min_num, nums[mid])
+
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return nums[left]
