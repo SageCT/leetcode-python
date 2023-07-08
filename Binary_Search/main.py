c66520358775d40efe2ece804a35cdf9ba144f74
SageCT <egas6554@gmail.com>
2023-07-07 20:01:43 -0500

    Updated problem153 and understand it

--- Binary_Search/main.py
+++ Binary_Search/main.py
@@ -10,12 +10,17 @@ class Solution:
         left, right = 0, len(nums) - 1
         min_num = nums[0]
 
-        while left < right:
+        while left <= right:
+            if nums[left] < nums[right]:
+                min_num = min(min_num, nums[left])
+                break
+
             mid = (left + right) // 2
             min_num = min(min_num, nums[mid])
 
-            if nums[mid] > nums[right]:
+            if nums[mid] >= nums[left]:
                 left = mid + 1
             else:
                 right = mid - 1
-        return nums[left]
+
+        return min_num
