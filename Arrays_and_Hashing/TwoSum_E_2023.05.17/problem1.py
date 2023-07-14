d751a8334e1aab85e1393454464d9c60ffa6fca5
SageCT <egas6554@gmail.com>
2023-07-13 20:24:59 -0500

    Reworked TwoSum and isPalindrome

--- Arrays_and_Hashing/TwoSum_E_2023.05.17/problem1.py
+++ Arrays_and_Hashing/TwoSum_E_2023.05.17/problem1.py
@@ -3,7 +3,17 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for x in range(len(nums)):
-            for y in range(x + 1, len(nums)):
-                if (nums[x] + nums[y]) == target:
-                    return [x, y]
+        
+        # * Solution using hashmap
+        hashmap = {}
+        for count, currNum in enumerate(nums):
+            diff = target - currNum
+            if diff in hashmap:
+                return [hashmap[diff], count]
+            hashmap[currNum] = count
+        
+        # * Brute Force Solution
+        # for x in range(len(nums)):
+        #     for y in range(x + 1, len(nums)):
+        #         if (nums[x] + nums[y]) == target:
+        #             return [x, y]
--- Two_Pointers/ValidPanlindrome_E_2023.06.11/problem125.py
+++ Two_Pointers/ValidPanlindrome_E_2023.06.11/problem125.py
@@ -1,10 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        def isPal(word):
-            return word == word[::-1]
-
-        if len(s) == 0:
-            return True
         s = "".join(i for i in s.lower() if i.isalnum() and i)
-
-        return isPal(s)
+        return s == s[::-1]
