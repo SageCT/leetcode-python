c6d8e569f4166b95ff0b1512b2f2aaf63fc23e07
SageCT <egas6554@gmail.com>
2023-07-19 15:29:14 -0500

    Completed ClimbingStairs

--- 1D_Dynamic_Programming/Climbing_Stairs_E_2023.07.15/problem70.py
+++ 1D_Dynamic_Programming/Climbing_Stairs_E_2023.07.15/problem70.py
@@ -1,3 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
+        # * You can use the fibbonaci sequence algo to complete this problem
+        if n <= 2:
+            return n
+
+        n -= 1
+        arr = []
+        arr.append(1)
+        arr.append(1)
+
+        while n >= 1:
+            arr.insert(0, arr[0] + arr[1])
+            n -= 1
+        return arr[0]
