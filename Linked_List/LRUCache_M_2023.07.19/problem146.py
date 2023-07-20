cf4eb9a8fd058863b9cf3fb75bf2f53dd3bac242
SageCT <egas6554@gmail.com>
2023-07-20 12:33:42 -0500

    Updated LRUCache to use ordereddict

--- Linked_List/LRUCache_M_2023.07.19/problem146.py
+++ Linked_List/LRUCache_M_2023.07.19/problem146.py
@@ -1,55 +1,26 @@
-class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
+from collections import OrderedDict
 
 
 class LRUCache:
     def __init__(self, capacity: int):
-        self.cache = {}
+        self.lru = OrderedDict()
         self.cap = capacity
 
-        # ? Linkedlist for determining the LRU nodes
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
-
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
-
-    # ? This funct gets the key in the params
-    # ? and moves it to the front of the LL
+    # ? If key is within lru ordered dict
+    # ? move the key to the end (most recently used)
+    # ? and return key
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
+        if key in self.lru:
+            self.lru.move_to_end(key)
+            return self.lru[key]
         return -1
 
-        if self.cache[key]:
-            return (key, self.cache[key])
+    # ? sets new item in dict and moves to the end
+    # ? (most recently used)
+    # ? If capacity is hit, popitem(False) for FIFO
 
-    # ? This funct puts a new node into the LL data struct
-    # ? if the node is already present or capacity is reached,
-    # ? then remove the old one and insert the new one
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-
-        if len(self.cache) > self.cap:
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        self.lru[key] = value
+        self.lru.move_to_end(key)
+        if len(self.lru) > self.cap:
+            self.lru.popitem(False)
