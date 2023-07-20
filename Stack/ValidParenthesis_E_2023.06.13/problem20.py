e2b3385a3af6eec99be79d40c11b4fa496b65840
SageCT <egas6554@gmail.com>
2023-07-19 19:07:03 -0500

    Added WordSearch

--- Stack/ValidParenthesis_E_2023.06.13/problem20.py
+++ Stack/ValidParenthesis_E_2023.06.13/problem20.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack: str = []
+        stack = []
         checker = {")": "(", "]": "[", "}": "{"}
         for c in s:
             if c in checker:
