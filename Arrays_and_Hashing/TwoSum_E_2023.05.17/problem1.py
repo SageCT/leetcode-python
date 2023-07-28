af5a771e8e97ff811e326313e12007563f5573cf
SageCT <egas6554@gmail.com>
2023-07-28 17:37:01 -0500

    Added WaysToSplitArrayInto3SubArr

--- Arrays_and_Hashing/TwoSum_E_2023.05.17/problem1.py
+++ Arrays_and_Hashing/TwoSum_E_2023.05.17/problem1.py
@@ -5,11 +5,15 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # * Solution using hashmap
         hashmap = {}
+        # ? For every digit in nums, if the difference between the
+        # ? Target and the current number is in the hashmap
+        # ? That means there is a possible combo
         for count, currNum in enumerate(nums):
             diff = target - currNum
             if diff in hashmap:
                 return [hashmap[diff], count]
             hashmap[currNum] = count
+        # * Not needed for problem
         return list(hashmap)
 
         # * Brute Force Solution
